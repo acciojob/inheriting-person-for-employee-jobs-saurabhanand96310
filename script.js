ef125bc93ae53981ac179239bc53f050ec7897e4
saurabhanand96310 <122476256+saurabhanand96310@users.noreply.github.com>
2024-10-18 01:04:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
-// complete this js code
 function Person(name, age) {
-		this.name=name;
-		this.age=age;
-}
-Person.prototype.greet = function () {
-	console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`)
+    this.name = name;
+    this.age = age;
 }
 
+Person.prototype.greet = function () {
+    console.log(`Hello, my name is ${this.name}, and I am ${this.age} years old.`);
+}
 
 function Employee(name, age, jobTitle) {
-	Person.call(this , name, age);
-	this.jobTitle=jobTitle;
+    Person.call(this, name, age); // Call the Person constructor
+    this.jobTitle = jobTitle;
 }
-Employee.prototype= Object.create(Person.prototype)
-Employee.prototype.constructor = Employee;
 
-Employee.prototype.jobGreet= function(){
-	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}`)
+// Inherit from Person
+Employee.prototype = Object.create(Person.prototype);
+Employee.prototype.constructor = Employee; // Correct the constructor reference
+
+Employee.prototype.jobGreet = function() {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
 }
 
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
+
