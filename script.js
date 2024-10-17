82f30199c0640379266a9974a4e67ce5c5d1ea00
saurabhanand96310 <122476256+saurabhanand96310@users.noreply.github.com>
2024-10-18 01:03:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ function Employee(name, age, jobTitle) {
 	this.jobTitle=jobTitle;
 }
 Employee.prototype= Object.create(Person.prototype)
+Employee.prototype.constructor = Employee;
 
 Employee.prototype.jobGreet= function(){
 	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}`)
