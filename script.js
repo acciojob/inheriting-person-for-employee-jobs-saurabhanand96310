9344cfedbed973b39b10b117b239bb13b149bb63
saurabhanand96310 <122476256+saurabhanand96310@users.noreply.github.com>
2024-10-18 01:29:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ Employee.prototype = Object.create(Person.prototype);
 Employee.prototype.constructor = Employee; // Correct the constructor reference
 
 Employee.prototype.jobGreet = function() {
-     console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}`)
+     console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
 }
 
 // Do not change code below this line
